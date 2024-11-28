14da64ae02b405e88fd55432706e23062f666414
mac <mac@Apples-MacBook-Pro.local>
2024-11-28 16:28:30 -0600

    Redesign press page and projects posts layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,44 +6,30 @@
  *
  */
 
+// Add this to ignore SSL errors for local development
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 module.exports = {
+  flags: {
+    LMDB_STORE: false,
+  },
   siteMetadata: {
     title: `StudiosC`,
     description: `Architecture Studio Based in Brooklyn, New York`,
     author: `StudiosC`,
     siteUrl: `https://studiosc.net`,
   },
-  /**
-   * Adding plugins to this array adds them to your Gatsby site.
-   *
-   * Gatsby has a rich ecosystem of plugins.
-   * If you need any more you can search here: https://www.gatsbyjs.com/plugins/
-   */
   plugins: [
     {
-      /**
-       * First up is the WordPress source plugin that connects Gatsby
-       * to your WordPress site.
-       *
-       * visit the plugin docs to learn more
-       * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
-       *
-       */
       resolve: `gatsby-source-wordpress`,
       options: {
-        // the only required plugin option for WordPress is the GraphQL url.
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://studiosc.theremotecreative.com/graphql`,
+        url: process.env.WPGRAPHQL_URL || `https://studiosc.theremotecreative.com/graphql`,
+        schema: {
+          requestConcurrency: 5,
+          previewRequestConcurrency: 2,
+        },
       },
     },
-
-    /**
-     * We need this plugin so that it adds the "File.publicURL" to our site
-     * It will allow us to access static url's for assets like PDF's
-     *
-     * See https://www.gatsbyjs.org/packages/gatsby-source-filesystem/ for more info
-     */
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -63,19 +49,12 @@ module.exports = {
         display: 'swap'
       },
     },
-
-    /**
-     * The following two plugins are required if you want to use Gatsby image
-     * See https://www.gatsbyjs.com/docs/gatsby-image/#setting-up-gatsby-image
-     * if you're curious about it.
-     */
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-smoothscroll`,
     {
-      // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `StudiosC`,
@@ -87,14 +66,7 @@ module.exports = {
         icon: `content/assets/logo-square.png`,
       },
     },
-
-    // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
     `gatsby-plugin-react-helmet`,
-
-    /**
-     * this (optional) plugin enables Progressive Web App + Offline functionality
-     * To learn more, visit: https://gatsby.dev/offline
-     */
     // `gatsby-plugin-offline`,
   ],
 }
--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,269 +1,416 @@
 import React from "react"
-import { graphql } from "gatsby"
-import styled from 'styled-components'
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 import { GatsbyImage } from "gatsby-plugin-image"
-import Slider from "react-slick"
-import scrollTo from 'gatsby-plugin-smoothscroll';
-
-import { GrTextAlignFull } from 'react-icons/gr';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const ProjectTemplate = ({ data: { post } }) => {
+const ProjectTemplate = ({ data }) => {
+  const post = data?.post
 
-    const slideMap = post.propertyInfo.propertyGallery
+  // Fallback if data is missing
+  if (!post) {
+    return <p>Project data is missing. Please check the query and context.</p>
+  }
 
-    const settings = {
-        arrows: true,
-        dots: false,
-        infinite: true,
-        autoplay: false,
-        speed: 1000,
-        fade: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
+  const featuredImage = post.seo.opengraphImage.localFile.childImageSharp.gatsbyImageData
 
-    return (
-        <Layout>
-            <Seo 
-            title={post.seo.title} 
-            description={post.seo.metaDesc}
-            metaImage={post.seo.opengraphImage.localFile.childImageSharp.fluid}
+  return (
+    <Layout>
+      <Seo
+        title={post.seo.title}
+        description={post.seo.metaDesc}
+        metaImage={featuredImage}
+      />
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <ProjectMain>
+          {/* Top Info Section */}
+          <div className="top-info">
+            <div className="top-info-left">
+              <h1>{post.title}</h1>
+              <p>{post.propertyInfo.propertyLocation}</p>
+            </div>
+            <div className="top-info-right">
+              <p>{post.propertyInfo.propertyShortDescription}</p>
+            </div>
+          </div>
+
+          {/* Featured Image */}
+          <div className="featured-image">
+            <GatsbyImage
+              className="featured-img"
+              image={featuredImage}
+              alt={post.title}
             />
-          <article
-            className="blog-post"
-            itemScope
-            itemType="http://schema.org/Article"
-          >
-            <ProjectMain>
+          </div>
 
-                <section class="slider-container">
-                    <Slider {...settings}>
-                        {slideMap.map(imageSrc => (
-                            <div class="project-slide">
-                                <GatsbyImage className={"slide-background"} image={imageSrc.localFile.childImageSharp.gatsbyImageData} alt={imageSrc.title} />
-                            </div>
-                        ))}
-                    </Slider>
-                </section>
+          {/* Property Info Section */}
+          <div className="property-info">
+            {/* Info Left */}
+            <div className="info-left">
+              <h2>About the Project</h2>
+              <div
+                className="description"
+                dangerouslySetInnerHTML={{
+                  __html: post.propertyInfo.propertyDescription,
+                }}
+              />
+            </div>
 
-                <header class="project-title">
-                    <div class="col-one header-col">
-        
-                    </div>
-                    <div class="col-two header-col">
-                        <h1 class="proj-title">{post.title}</h1>
-                    </div>
-                    <div class="col-three header-col">
-                        <button onClick={() => scrollTo('#proj_cont')}><GrTextAlignFull size={24}/></button>
-                    </div>
-                </header>
+            {/* Info Right */}
+            <div className="info-right">
+              <h2>Project Details:</h2>
+              <ul>
+                <li>
+                  <p>Client</p> {post.propertyInfo.propertyClient}
+                </li>
+                <li>
+                  <p>Location</p> {post.propertyInfo.propertyLocation}
+                </li>
+                <li>
+                  <p>Size</p> {post.propertyInfo.propertySize}
+                </li>
+                <li>
+                  <p>Date</p> completed {post.propertyInfo.propertyDate}
+                </li>
+                <li className="tag-list">
+                  <p>Tags</p>
+                  <ul className="tags">
+                    {post.tags.nodes.map(tag => (
+                      <li key={tag.id}>{tag.name}</li>
+                    ))}
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
 
-                <section id="proj_cont" class="project-content" >
-                    <div class="row">
-                        <ul class="description-titles">
-                            <li>Client</li>
-                            <li>Location</li>
-                            <li>size</li>
-                            <li>date completed</li>
-                        </ul>
-                        <ul class="description-info">
-                            <li>{post.propertyInfo.propertyClient}</li>
-                            <li>{post.propertyInfo.propertyLocation}</li>
-                            <li>{post.propertyInfo.propertySize}</li>
-                            <li>{post.propertyInfo.propertyDate}</li>
-                        </ul>
-                        <div class="description-main" dangerouslySetInnerHTML={{__html: post.propertyInfo.propertyDescription}} />
-                    </div>
-                </section>
+          {/* Gutenberg Content */}
+          <div className="gutenberg-content">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.content, // Render Gutenberg content
+              }}
+            />
+          </div>
 
-            </ProjectMain>
-    
-          </article>
-        </Layout>
-      )
-
+          {/* Back to Projects Button */}
+          <div className="back-to-projects">
+            <Link to="/projects">Back to Projects</Link>
+          </div>
+        </ProjectMain>
+      </article>
+    </Layout>
+  )
 }
 
-
 const ProjectMain = styled.div`
-    max-width: 1230px;
-    width: 100%;
-    padding: 0 50px;
-    margin: 50px auto;
-    margin-top: 0;
-    .slider-container {
-        .slick-prev {
-            width: 30px;
-            height: 30px;
-            left: 30px;
-            border-top: 6px solid #fff;
-            border-left: 6px solid #fff;
-            transform: rotate(-45deg);
-            z-index: 2;
-            color: transparent;
-            box-shadow: -1px -1px 2px rgb(0 0 0 / 20%);
-            &:before {
-                display: none;
-            }
-        }
-        .slick-next {
-            width: 30px;
-            height: 30px;
-            right: 30px;
-            border-top: 6px solid #fff;
-            border-right: 6px solid #fff;
-            transform: rotate(45deg);
-            z-index: 2;
-            color: transparent;
-            box-shadow: 1px -1px 2px rgb(0 0 0 / 20%);
-            &:before {
-                display: none;
-            }
-        }
-        .gatsby-image-wrapper {
-            width: 100%;
-        }
-    }
-    .project-title {
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .col-one {
-            width: 30%;
-        }
-        .col-two {
-            width: 40%;
-            text-align: center;
-        }
-        .col-three {
-            width: 30%;
-            display: flex;
-            justify-content: flex-end;
-            align-items: center;
-        }
-        h1 {
-            font-family: 'Roboto', sans-serif;
-            font-weight: 400;
-            text-transform: uppercase;
-            font-size: 26px;
-            padding: 30px;
-            max-width: 100%;
-            margin: 0;
-            text-align: center;
-        }
-        button {
-            background-color: transparent;
-            border: none;
-            &:hover {
-                cursor: pointer;
-            }
-        }
-    }
-    .project-content {
-        width: 100%;
-        margin: 50px 0 150px;
-        .row {
-            display: flex;
-            justify-content: center;
-            align-items: flex-start;
-        }
-        .description-titles {
-            width: 16.66%;
-            color: #b2b2b2;
-            list-style: none;
-            margin: 0;
-            li {
-                font-family: Roboto;
-                font-weight: 900;
-                font-size: 12px;
-                text-transform: uppercase;
-                border-top: 1px solid #e7e7e7;
-                padding: 7px 0;
-                margin: 0;
-                &:last-child {
-                    border-bottom: 1px solid #e7e7e7;
-                }
-            }
-        }
-        .description-info {
-            width: 16.66%;
-            color: #242424;
-            list-style: none;
-            margin: 0;
-            li {
-                font-family: Roboto;
-                font-weight: 900;
-                font-size: 12px;
-                text-transform: uppercase;
-                border-top: 1px solid #e7e7e7;
-                padding: 7px 0;
-                margin: 0;
-                &:last-child {
-                    border-bottom: 1px solid #e7e7e7;
-                }
-            }
-        }
-        .description-main {
-            width: 66.66%;
-            padding-left: 20px;
-            p {
-                font-family: 'Lato', sans-serif;
-                font-size: 13px;
-            }
-        }
-    }
-    @media(max-width:767px) {
-        padding: 0px;
+  width: 100%;
+  padding: 0 50px;
+  margin: 50px auto;
+
+  .top-info {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+
+    .top-info-left {
+      flex: 1.6;
+
+      h1 {
+        font-size: 42px;
+        font-weight: bold;
+        margin-bottom: 10px;
         margin-top: 0;
-        .project-title {
-            .col-one,
-            .col-three {
-                display: none;
-            }
-            .col-two {
-                width: 100%;
-            }
-        }
-        .project-content {
-            padding: 0 20px;
-            .row {
-                flex-wrap: wrap;
-                .description-titles,
-                .description-info {
-                    width: 50%;
-                }
-                .description-main {
-                    width: 100%;
-                    margin-top: 20px;
-                    padding-left: 0px;
-                }
-            }
-        }
-        
+      }
+
+      p {
+        font-size: 16px;
+        color: #555;
+      }
     }
+
+    .top-info-right {
+      flex: 1;
+
+      p {
+        font-size: 14px;
+        color: #555;
+      }
+    }
+  }
+
+  .featured-image {
+    margin-bottom: 30px;
+
+    .gatsby-image-wrapper {
+      width: calc(100% + 100px);
+      margin-left: -50px;
+      max-height: 100vh;
+    }
+  }
+
+  .property-info {
+    display: grid;
+    grid-template-columns: 1fr 0.6fr 0.8fr;
+    gap: 20px;
+    margin-top: 50px;
+
+    .info-left {
+      h2 {
+        font-size: 24px;
+        font-weight: bold;
+        margin-bottom: 30px;
+      }
+
+      .description {
+        font-size: 14px;
+        color: #333;
+      }
+    }
+
+    .info-right {
+      grid-column: 3;
+      justify-self: end;
+      width: 100%;
+      padding-left: 20px;
+
+      h2 {
+        font-size: 16px;
+        font-weight: bold;
+        margin-bottom: 22px;
+        padding: 0 8px;
+      }
+
+      ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+
+        li {
+          display: flex;
+          justify-content: space-between;
+          font-size: 14px;
+          padding: 17px 8px;
+          border-top: 1px solid #5F5F5F;
+          margin-top: 0px;
+          margin-bottom: 0px;
+
+          strong {
+            font-weight: bold;
+            color: #555;
+          }
+
+          .tags {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 2px;
+            margin-top: 10px;
+
+            li {
+              background: #111111;
+              padding: 5px 10px;
+              font-size: 12px;
+              margin: 3px;
+              color: #ffffff;
+              border: none;
+            }
+          }
+        }
+
+        .tag-list {
+          display: block; /* Explicitly set display block for the tag-list class */
+        }
+      }
+    }
+  }
+
+  .gutenberg-content {
+    margin-top: 40px;
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: "Roboto", sans-serif;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+
+    p {
+      font-size: 14px;
+      line-height: 1.6;
+      margin-bottom: 20px;
+    }
+
+    ul, ol {
+      margin-left: 20px;
+      margin-bottom: 20px;
+    }
+
+    a {
+      color: #007acc;
+      text-decoration: underline;
+
+      &:hover {
+        color: #005fa3;
+      }
+    }
+  }
+
+  .back-to-projects {
+    margin-top: 30px;
+    text-align: right;
+
+    a {
+      display: inline-block;
+      background-color: #111111;
+      color: #fff;
+      text-decoration: none;
+      padding: 10px 16px;
+      font-size: 14px;
+      transition: background-color 0.3s;
+      font-weight: 600;
+
+      &:hover {
+        background-color: #333;
+      }
+    }
+  }
+
+  .two-column-grid{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1em;
+  }
+
+  .one-em-gap {
+    gap: 1em;
+  }
+
+  .margin-top-80{
+    margin-top: 80px;
+  }
+
+  .padding-right-10-percent{
+    padding-right: 10%;
+  }
+
+  @media (max-width: 992px) {
+
+    .property-info{
+        grid-template-columns: 1fr 0.2fr 0.8fr;
+    }
+
+  }
+
+  @media (max-width: 781px){
+
+    .wp-block-image{
+        text-align: center;
+    }
+
+    .wp-block-column:nth-child(even) {
+        margin-left: 0;
+    }
+
+    .wp-block-column {
+        flex-basis: 100% !important;
+    }
+
+    .margin-top-80, .wp-block-columns{
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    .two-column-grid{
+        gap: 0;
+    }
+
+  }
+
+  @media (max-width: 768px) {
+    padding: 0px;
+
+    .top-info {
+      flex-direction: column;
+      padding: 0 50px;
+
+      .top-info-right {
+        text-align: left;
+        margin-top: 10px;
+      }
+    }
+
+    .property-info {
+        grid-template-columns: 1fr;
+        gap: 0;
+        padding: 0 50px;
+
+      .info-right{
+        grid-column: 1;
+      }
+
+      .info-left,
+      .info-right {
+        border: none;
+        padding: 0;
+
+        h2 {
+          font-size: 18px;
+          margin-top: 20px;
+        }
+      }
+
+      .info-right ul li {
+        justify-content: space-between;
+      }
+    }
+
+    .two-column-grid{
+        grid-template-columns: 1fr;
+    }
+
+    .back-to-projects{
+        text-align: center;
+    }
+
+    .mobile-padding{
+        padding: 0 50px;
+    }
+  }
+
+  @media (max-width: 480px){
+    
+    .mobile-padding, .property-info, .top-info{
+        padding: 0 20px;
+    }
+
+
+  }
 `
 
 export default ProjectTemplate
 
 export const pageQuery = graphql`
-  query ProjectById(
-    # these variables are passed in via createPage.pageContext in gatsby-node.js
-    $id: String!
-  ) {
-    # selecting the current page by id
+  query ProjectById($id: String!) {
     post: wpProperty(id: { eq: $id }) {
       id
       title
+      content # Gutenberg content
       seo {
         title
         metaDesc
         opengraphImage {
           localFile {
             childImageSharp {
+              gatsbyImageData(
+                width: 1130
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
               fluid(maxWidth: 1920) {
                 ...GatsbyImageSharpFluid_withWebp
               }
@@ -277,17 +424,12 @@ export const pageQuery = graphql`
         propertyLocation
         propertySize
         propertyDate
-        propertyGallery {
-          title
-          localFile {
-            childImageSharp {
-                gatsbyImageData (
-                    width: 1130
-                    placeholder: BLURRED
-                    formats: [AUTO, WEBP, AVIF]
-                )
-            }
-          }
+        propertyShortDescription
+      }
+      tags {
+        nodes {
+          id
+          name
         }
       }
     }
